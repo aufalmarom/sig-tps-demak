cb6ffd07b8dc280411350d1cf73eb2fb20118786
Aufal Marom <aufalmarom23@gmail.com>
2019-08-01 17:10:02 +0700

    add image about us

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -11,16 +11,14 @@
                 <h6 class="m-0 font-weight-bold text-primary">Peneliti</h6>
             </div>
             <div class="card-body">
+                <div class="text-center mb-3">
+                    <img class="text-center" src="{{asset('assets/img/kiki.jpeg')}}" style="max-height:600px" alt="">
+                </div>
                 <p class="text-center mb-0">Nama : Rizqi Umi Rahmawati</p>
                 <p class="text-center mb-0">Tempat/Tanggal Lahir : Demak/13 Agustus 1997</p>
                 <p class="text-center mb-0">Pendidikan : Teknik Geodesi, Universitas Diponegoro</p>
                 <p class="text-center mb-0">Email : rizqiumirahmawati@gmail.com</p>
-                <p class="text-center mt-3">Terimakasih telah mengunjungi website “Suara Demak 2019”, Kritik dan saran saya tunggu pada email berikut rizqiumirahmawati@gmail.com</p>
-                
-
-
-
-
+                <p class="text-center mt-3">Terimakasih telah mengunjungi website “Suara Demak 2019”. Kritik dan saran saya tunggu pada email berikut rizqiumirahmawati@gmail.com</p>
             </div>
         </div>
     </div>
